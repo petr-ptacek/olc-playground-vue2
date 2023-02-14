78d7e7025153782eaa08a49c386eecd70a1b3019
Petr Ptacek <petr.ptacek99@gmail.com>
2023-02-14 23:33:58 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="layout">
+    <TheCounter />
+  </div>
+</template>
+
+<script>
+import TheCounter from "@/components/Counter.vue";
+
+export default {
+  name: "App",
+  components: {
+    TheCounter
+  }
+}
+</script>
+
+<style>
+.layout {
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
+}
+</style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="counter">
+    <div class="counter__value">{{ counter }}</div>
+    <div class="counter__actions">
+      <button @click="increment">Increment</button>
+      <button @click="decrement">Decrement</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TheCounter",
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    increment() {
+      this.counter++;
+    },
+    decrement() {
+      this.counter--;
+    }
+  }
+}
+</script>
+
+<style scoped>
+.counter {
+  color: black;
+  background: lightblue;
+  padding: 20px;
+  max-width: 250px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  border-radius: 4px;
+}
+
+.counter__value {
+  font-size: 25px;
+  font-weight: 700;
+}
+
+.counter__actions {
+  display: flex;
+  gap: 10px;
+}
+
+button {
+  padding: 4px;
+  min-width: 100px;
+}
+</style>
